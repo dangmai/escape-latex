db59849d0c542dcd6b06868fe5182ac5d05a9aaf
Dang Mai <contact@dangmai.net>
2012-10-30 09:25:22 -0400

    Minor enhancement to comments and algorithm
    Stop making new regexps everytime escape function is run

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*jslint node:true*/
+"use strict";
+
 // Map the characters to escape to their escaped values. The list is derived
 // from http://www.cespedes.org/blog/85/how-to-escape-latex-special-characters
 var escapes = {
@@ -12,22 +15,24 @@ var escapes = {
     '_': '\\_',
     '~': '\\textasciitilde{}'
 },
-    escapeKeys = Object.keys(escapes); // as it is reused later on
-
-/**
- * Escape a string to be used in JS regular expression.
- * Code from http://stackoverflow.com/a/6969486
- * @param str the string to be used in a RegExp
- * @return the escaped string, ready to be used for RegExp
- */
-function escapeRegExp(str) {
-    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-}
+    /**
+     * Escape a string to be used in JS regular expression.
+     * Code from http://stackoverflow.com/a/6969486
+     * @param str the string to be used in a RegExp
+     * @return the escaped string, ready to be used for RegExp
+     */
+    escapeRegExp = function (str) {
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    },
+    escapeKeys = Object.keys(escapes), // as it is reused later on
+    escapeKeyRegExps = escapeKeys.map(function (key) {
+        return escapeRegExp(key);
+    });
 
 /**
  * Escape a string to be used in LaTeX documents.
  * @param str the string to be escaped.
- * return the escaped string, ready to be used in LaTeX.
+ * @return the escaped string, ready to be used in LaTeX.
  */
 function lescape(str) {
     var pos, match, regExp,
@@ -38,7 +43,6 @@ function lescape(str) {
     // We can't just sequentially replace each character(s), because the result
     // of an earlier step might be escaped again by a later step.
     escapeKeys.forEach(function (key, index) {
-        // TODO: enhance this so as not to construct a new RegExp everytime.
         if (regExpFound) {
             // This is here to avoid breaking up strings unnecessarily: In every
             // repetition step, we only need to find ONE special character(s) to
@@ -46,9 +50,8 @@ function lescape(str) {
             // further.
             return;
         }
-        regExp = new RegExp(escapeRegExp(key));
-        pos = str.search(regExp);
-        match = str.match(regExp);
+        pos = str.search(escapeKeyRegExps[index]);
+        match = str.match(escapeKeyRegExps[index]);
         if (pos !== -1) {
             result = lescape(str.slice(0, pos)) + escapes[escapeKeys[index]] + lescape(str.slice(pos + match.length));
             regExpFound = true;
